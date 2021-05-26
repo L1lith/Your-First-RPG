d606130ffa45193f9431edf4b499d13d916a37fc
L1lith <lily@l1lith.com>
2021-05-26 01:15:12 -0600

    try to fix the docs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withSass = require('@zeit/next-sass')
 
+console.log('Base Path:', process.env.NODE_ENV === 'development' ? '' : '/Intro-Coding-Concepts')
+
 module.exports = withSass({
   basePath: process.env.NODE_ENV === 'development' ? '' : '/Intro-Coding-Concepts'
 })
