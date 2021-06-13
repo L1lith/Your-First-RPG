4362d1a1d3c152bbae0976259b38ba4f98d7628e
BuildTools <lily@l1lith.com>
2021-06-13 05:19:52 -0600

    redeploy the docs

---
+++ docs/_next/static/webpack/styles.f07ba84e23ff2fff47dc.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./pages/index.scss":
+false,
+
+/***/ "./pages/resources.scss":
+false,
+
+/***/ 4:
+false,
+
+/***/ 7:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuZjA3YmE4NGUyM2ZmMmZmZjQ3ZGMuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
