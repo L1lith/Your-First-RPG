f35cfe7ebf2a8abb89969a1a07e34058f4849585
BuildTools <lily@l1lith.com>
2021-11-06 20:06:01 -0600

    rebuild the site

---
+++ docs/_next/static/webpack/styles.0c723d2459f329c0171a.hot-update.js
@@ -0,0 +1,22 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/BooleanGame/index.scss":
+false,
+
+/***/ "./pages/index.scss":
+false,
+
+/***/ "./pages/resources.scss":
+false,
+
+/***/ 11:
+false,
+
+/***/ 12:
+false,
+
+/***/ 5:
+false
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9zdHlsZXMuMGM3MjNkMjQ1OWYzMjljMDE3MWEuaG90LXVwZGF0ZS5qcyIsInNvdXJjZVJvb3QiOiIifQ==
