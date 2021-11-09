ac9e0a986f233fab5b3baabbd99ba1f411713c2c
BuildTools <lily@l1lith.com>
2021-11-08 19:07:30 -0700

    fix the favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,15 @@ import '../styles/_normalize.scss'
 import '../styles/_global.scss'
 import { DefaultSeo } from 'next-seo'
 import { Fragment } from 'react'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <DefaultSeo titleTemplate="%s - YFRPG" defaultTitle="Your First RPG"></DefaultSeo>
+      <Head>
+        <link rel="shortcut icon" href="/images/site-icon.png" />
+      </Head>
       <Header />
       <main className={'page ' + styles.page}>
         <Component {...pageProps} />
