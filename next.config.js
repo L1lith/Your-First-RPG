a09745210cfed088c886df1ffebca0c806bc358f
BuildTools <lily@l1lith.com>
2021-11-14 05:49:55 -0700

    redeploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   // other next config
   pwa: {
+    runtimeCaching,
     dest: 'public'
   },
   buildExcludes: [/middleware-manifest\.json$/, /_middleware.js$/, /_middleware.js.map$/]
