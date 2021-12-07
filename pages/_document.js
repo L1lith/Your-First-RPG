a81af8298b0ccb27ee2825f3701502b7c7ef1407
BuildTools <lily@l1lith.com>
2021-12-07 12:56:27 -0700

    add opengraph metadata

--- pages/_document.js
+++ pages/_document.js
@@ -8,14 +8,14 @@ class MyDocument extends Document {
         <DefaultSeo titleTemplate="%s - YFRPG" defaultTitle="Your First RPG"></DefaultSeo>
         <DefaultSeo description="Learn to make games in JavaScript for beginners" />
         <Head>
-          <link rel="shortcut icon" href="/images/site-icon.png" />
+          <link rel="shortcut icon" href="/images/site-icon.svg" />
           <meta
             httpEquiv="Content-Security-Policy"
-            content="default-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src youtube.com www.youtube.com;"
+            content="default-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src youtube.com www.youtube.com;"
           />
           <meta name="application-name" content="Your First RPG" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/images/app-icon.png" />
+          <link rel="apple-touch-icon" href="/images/site-icon.svg" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content="Your First RPG" />
@@ -25,6 +25,12 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#ffffff" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Your First RPG" />
+          <meta property="og:description" content="Learn to code by making a text RPG game" />
+          <meta property="og:site_name" content="Your First RPG" />
+          <meta property="og:url" content="https://yourfirstrpg.com" />
+          <meta property="og:image" content="https://yourfirstrpg.com/images/site-icon.svg" />
         </Head>
         <body>
           <Main />
