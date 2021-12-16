b7872f3ff05b01a44112e497a6e3fcc7bac19578
BuildTools <lily@l1lith.com>
2021-12-16 14:44:48 -0700

    preload the font

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
             httpEquiv="Content-Security-Policy"
             content="default-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; frame-src youtube.com www.youtube.com; worker-src 'self';"
           />
+          <link rel="preload" href="/fonts/droidSerifRegular.ttf" as="font" type="font/ttf" />
           <meta name="application-name" content="Your First RPG" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/images/icons/site-icon.svg" />
